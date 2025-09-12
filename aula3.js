38eb70235b45824855e9393dae17e2b87d32e70c
Caiokl3i <caio.victor.santos12@gmail.com>
2025-09-12 00:40:43 -0400

    feat: implementar função de verificação de quantidade

--- aula3.js
+++ aula3.js
@@ -2,109 +2,99 @@ const prompt = require("prompt-sync")({ sigint: true }); // sigint: true allows 
 
 //  somar, subtrair, multiplicar e dividir.
 
-console.clear()
+function verificacaoQtde(qtde, operacao) {
+    while (true) {
+        qtde = parseInt(prompt(`Quantos números deseja ${operacao}? (min 2)`));
+        if (qtde > 1) {
+            break
+        }
+        console.log("\nEscolha no mínimo 2 números !!\n");
+    }
+    return qtde
+}
+console.clear();
 while (true) {
-    console.log('Qual operação deseja fazer? \n1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão \n5 - sair')
-    let option = parseInt(prompt())
-    
-    console.clear()
+    console.log(
+        "Qual operação deseja fazer? \n1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão \n5 - sair"
+    );
+    let option = parseInt(prompt());
 
-    let qtdeNum = 0
+    console.clear();
+
+    let qtdeNum = 0;
 
     while (true) {
         switch (option) {
             case 1:
-                while (true) {
-                    qtdeNum = parseInt(prompt("Quantos números deseja somar? (min 2)"))
-                    if (qtdeNum > 1) {
-                        break
-                    }
-                    console.log("\nEscolha no mínimo 2 números !!\n")
-                }
+                qtdeNum = verificacaoQtde(qtdeNum, operacao = "somar")
 
-                let soma = 0
+                let soma = 0;
                 for (let i = 0; i < qtdeNum; i++) {
-                    let valor = parseInt(prompt(`Digite o ${i + 1}° numero?`))
+                    let valor = parseInt(prompt(`Digite o ${i + 1}° numero?`));
                     if (soma == 0) {
-                        soma = valor
+                        soma = valor;
                     } else {
-                        soma += valor
+                        soma += valor;
                     }
                 }
-                console.log(`A soma de todos os números é ${soma}`)
-                console.log()
-                break
+                console.log(`A soma de todos os números é ${soma}`);
+                console.log();
+                break;
 
             case 2:
-                while (true) {
-                    qtdeNum = parseInt(prompt("Quantos números deseja subtrair? (min 2)"))
-                    if (qtdeNum > 1) {
-                        break
-                    }
-                    console.log("\nEscolha no mínimo 2 números !!\n")
-                }
+                qtdeNum = verificacaoQtde(qtdeNum, operacao="subtrair")
 
-                let subtracao = 0
+                let subtracao = 0;
                 for (let i = 0; i < qtdeNum; i++) {
-                    let valor = parseInt(prompt(`Digite o ${i + 1}° numero?`))
+                    let valor = parseInt(prompt(`Digite o ${i + 1}° numero?`));
                     if (subtracao == 0) {
-                        subtracao += valor
+                        subtracao += valor;
                     } else {
-                        subtracao -= valor
+                        subtracao -= valor;
                     }
                 }
-                console.log(`A subtração de todos os números é ${subtracao}`)
-                console.log()
+                console.log(`A subtração de todos os números é ${subtracao}`);
+                console.log();
                 break;
 
             case 3:
-                while (true) {
-                    qtdeNum = parseInt(prompt("Quantos números deseja multiplicar? (min 2)"))
-                    if (qtdeNum > 1) {
-                        break
-                    }
-                    console.log("\nEscolha no mínimo 2 números !!\n")
-                }
-                let multiplicacao = 0
+                qtdeNum = verificacaoQtde(qtdeNum, operacao="multiplicar")
+
+                let multiplicacao = 0;
                 for (let i = 0; i < qtdeNum; i++) {
-                    let valor = parseInt(prompt(`Digite o ${i + 1}° numero?`))
+                    let valor = parseInt(prompt(`Digite o ${i + 1}° numero?`));
                     if (multiplicacao == 0) {
-                        multiplicacao = valor
+                        multiplicacao = valor;
                     } else {
-                        multiplicacao *= valor
+                        multiplicacao *= valor;
                     }
                 }
-                console.log(`A multiplicação de todos os números é ${multiplicacao}`) 
-                console.log()
+                console.log(`A multiplicação de todos os números é ${multiplicacao}`);
+                console.log();
                 break;
 
             case 4:
-                while (true) {
-                    qtdeNum = parseInt(prompt("Quantos números deseja dividir? (min 2)"))
-                    if (qtdeNum > 1) {
-                        break
-                    }
-                    console.log("\nEscolha no mínimo 2 números !!\n")
-                }
-                let divisao = 0
+                qtdeNum = verificacaoQtde(qtdeNum, operacao="dividir")
+
+                let divisao = 0;
                 for (let i = 0; i < qtdeNum; i++) {
-                    let valor = parseInt(prompt(`Digite o ${i + 1}° numero?`))
+                    let valor = parseInt(prompt(`Digite o ${i + 1}° numero?`));
                     if (divisao == 0) {
-                        divisao = valor
+                        divisao = valor;
                     } else {
-                        divisao /= valor
+                        divisao /= valor;
                     }
                 }
-                console.log(`A divisão de todos os números é ${divisao}`)
-                console.log()
+                console.log(`A divisão de todos os números é ${divisao}`);
+                console.log();
                 break;
             case 5:
-                break
+                break;
         }
-        break
+        break;
     }
     if (option == 5) {
-        console.log('Encerrando calculadora . . .')
-        break
+        console.log("Encerrando calculadora . . .");
+        break;
     }
 }
