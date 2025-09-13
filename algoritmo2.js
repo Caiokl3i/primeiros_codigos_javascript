2fbcd7f7f6bfb181e5e650c09fd75909dd6e7a80
Caiokl3i <caio.victor.santos12@gmail.com>
2025-09-13 17:22:41 -0400

    feat(operacaoIguais): implementa função para operação de numeros iguais

---
+++ algoritmo2.js
@@ -0,0 +1,13 @@
+// Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem
+// iguais, deverá somar os dois valores, caso contrário deverá multiplicar A por B.Ao
+// final de qualquer um dos cálculos deve - se atribuir o resultado a uma variável C e imprimir
+// seu valor na tela.
+
+function operacaoIguais(a, b) {
+    if (a == b) {
+        return a + b
+    } else {
+        return a * b
+    }
+}
+
