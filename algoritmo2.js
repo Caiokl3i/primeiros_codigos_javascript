d207affc252f30713fff84e02a7995ed6cb1a2f3
Caiokl3i <caio.victor.santos12@gmail.com>
2025-09-13 17:38:16 -0400

    feat: adiciona interação com o usuário via prompt

--- algoritmo2.js
+++ algoritmo2.js
@@ -11,3 +11,22 @@ function operacaoIguais(a, b) {
     }
 }
 
+const prompt = require("prompt-sync")({ sigint: true }); // sigint: true allows Ctrl+C to exit
+
+let a, b, c
+
+while (true) {
+    a = Number(prompt("Digite o valor de A número: "))
+
+    b = Number(prompt("Digite o valor de B número: "))
+
+    if (Number.isNaN(a) || Number.isNaN(b)) {
+        console.log('Número invalido !!')
+    } else {
+        break
+    }
+}
+
+c = operacaoIguais(a, b)
+
+console.log(`Valor de C: ${c}`)
