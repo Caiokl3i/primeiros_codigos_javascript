82f0a21e88a30f8bda9af1164063e9fc2a021c74
Caiokl3i <caio.victor.santos12@gmail.com>
2025-09-13 15:25:37 -0400

    feat(verifNum): implementa função de verificação de número

--- algoritmo1.js
+++ algoritmo1.js
@@ -1,2 +1,22 @@
 // receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.
 
+function verifNum(num){
+    opcoes = ['par', 'impar', 'positivo', 'negativo', 'neutro']
+    caracteristicasNum = []
+
+    if (num > 0 ) {
+        caracteristicasNum.push(opcoes[2])
+    } else if (num == 0){
+        caracteristicasNum.push(opcoes[4])
+    } else {
+        caracteristicasNum.push(opcoes[3])
+    }
+
+    if (num % 2 == 0) {
+        caracteristicasNum.push(opcoes[0])
+    } else {
+        caracteristicasNum.push(opcoes[1])
+    }
+
+    return caracteristicasNum
+}
