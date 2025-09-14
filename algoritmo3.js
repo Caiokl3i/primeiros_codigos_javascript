f90bb0d1c3b6978e472abb017edf1fadcf90fe43
Caiokl3i <caio.victor.santos12@gmail.com>
2025-09-14 16:26:01 -0400

    feat: adiciona interação com o usuário via prompt

--- algoritmo3.js
+++ algoritmo3.js
@@ -8,3 +8,22 @@ function antecSucess(num) {
 
     return [antecessor, sucessor]
 }
+
+const prompt = require("prompt-sync")({ sigint: true }); // sigint: true allows Ctrl+C to exit
+
+let num
+
+while (true) {
+    num = Number(prompt("Digite um número: "))
+
+    if (Number.isNaN(num)) {
+        console.log('Número invalido !!')
+    } else {
+        break
+    }
+}
+
+result = antecSucess(num)
+
+console.log(`O antecessor e o sucessor do número ${num} são, respectivamente: ${result.join(" e ")}`)
+
