eb66f698d7f4988556fa10b9c3b055c841b7b531
Caiokl3i <caio.victor.santos12@gmail.com>
2025-09-14 16:17:16 -0400

    feat(anteSucess): implementa função que calcula antecessor e sucessor

--- algoritmo3.js
+++ algoritmo3.js
@@ -1,2 +1,10 @@
 // Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.
 
+function antecSucess(num) {
+    let antecessor, sucessor
+
+    antecessor = num - 1
+    sucessor = num + 1
+
+    return [antecessor, sucessor]
+}
