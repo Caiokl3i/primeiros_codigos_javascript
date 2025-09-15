17e0c32dacd016994167ba9105914a72ad61d8d2
Caiokl3i <caio.victor.santos12@gmail.com>
2025-09-14 22:29:29 -0400

    feat: cria interação com o usuário via prompt

--- algoritmo4.js
+++ algoritmo4.js
@@ -5,6 +5,29 @@
 function calculaSalario(salario) {
     let qtdeSalarios = salario / 1500
 
-    return Number(qtdeSalarios)
+    return qtdeSalarios
 }
 
+const prompt = require("prompt-sync")({ sigint: true }); // sigint: true allows Ctrl+C to exit
+
+let salario
+
+while (true) {
+    salario = Number(prompt('Digite o valor do seu salário: '))
+
+    if (Number.isNaN(salario)) {
+        console.log("Número inválido !!")
+    } else {
+        break
+    }
+}
+
+qtdeSalarios = calculaSalario(salario)
+
+if (qtdeSalarios < 1) {
+    console.log(`Você ganha menos de um salario minimo. Equivale a ${qtdeSalarios.toFixed(2)}`)
+} else if (qtdeSalarios == 1){
+    console.log(`Você ganha exatamente ${qtdeSalarios.toFixed(0)} salário mínimo`)
+} else {
+    console.log(`Seu salário equivale a ${qtdeSalarios.toFixed(2)} salários mínimos`)
+}
